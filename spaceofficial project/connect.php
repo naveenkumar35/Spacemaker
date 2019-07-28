a3e1f7101bd6dbe11c972122f68c39b67e911148
Naveenkumar A S <46211311+naveenkumar35@users.noreply.github.com>
2019-07-28 07:11:41 -0700

    Add files via upload

---
+++ spaceofficial project/connect.php
@@ -0,0 +1,12 @@
+
+<?php
+
+define('HOST','localhost');
+ define('USERNAME', 'id10319915_root');
+ define('PASSWORD','naveen1999');
+ define('DB','id10319915_spaceofficial');
+
+mysql_connect($hostname, $username, $password) OR DIE('Unable to connect to database! Please try again later.');
+mysql_select_db($dbname);
+
+?>
---
+++ spaceofficial project/signup.php
@@ -0,0 +1,33 @@
+<?php
+define('HOST','localhost');
+ define('USERNAME', 'id10319915_root');
+ define('PASSWORD','naveen1999');
+ define('DB','id10319915_spaceofficial');
+
+// Create connection
+$con = mysqli_connect(HOST,USERNAME,PASSWORD,DB);
+
+// Check connection
+$first = $_POST['fn'];
+$last = $_POST['ln'];
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
+//$sql = "insert into details values ('$user','$pass')";
+$sq = "insert into details values ('$first','$last','$user','$pass')";
+$sql1 = "SELECT * from details WHERE usname LIKE '{$user}' AND pass LIKE '{$pass}' LIMIT 1";
+$result = $con->query($sql1);
+if ($result->num_rows == 1) 
+    {
+    
+    header('Location: redindex.html');
+    exit;
+    } 
+else
+{if(mysqli_query($con, $sq)){// && mysqli_query($con,$sql)){
+  header('Location: login.html');
+    exit;
+ }
+}
+
+?>
